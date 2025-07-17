90404f809b059ae4dd37273722835fde25e68563
ChandniBhadarka <chandnibhadarka@gmail.com>
2025-07-17 23:07:13 +0530

    commit

--- main.py
+++ main.py
@@ -10,11 +10,23 @@ class Employee:
     def increase(self):
         self.salary = self.salary * self.increament
         #self.salary = self.salary * Employee.increament
+
+    @classmethod
+    def change_increament(cls, amount):
+        cls.increament = amount
+
      
 
 
 vivek = Employee('vivek', 'vyas', '25000')
 dhruv = Employee('dhruv', 'das', '25000')
+
+#class method
+print(vivek.salary)
+Employee.change_increament(3)
+vivek.increase()
+print(vivek.salary)
+
 
 vivek.increase() # 0 agruement but self is defualt so 1 arguement 
 #koi object me function ko call krege toh "self" automatically pass ho jaega
